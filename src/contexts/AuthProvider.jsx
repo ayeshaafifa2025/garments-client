ae02b21bb225ec9bd28295e94986d0c448d063d2
ayeshaafifa2025 <ayeshaafifa2025@gmail.com>
2025-12-14 17:08:56 +0600

    created auth provider.jsx

---
+++ src/contexts/AuthProvider.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+const AuthProvider = ({ children }) => {
+
+
+    const authInfo = {
+       
+    }
+
+    return (
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
